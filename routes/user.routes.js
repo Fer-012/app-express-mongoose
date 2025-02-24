1a2e9bbff66685407d46774f835d4f2ba2a50b5d
Fer-012 <aouachriferdaoues@gmail.com>
2025-02-24 11:14:54 +0100

    Initial commit with project structure and configuration

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,13 +2,17 @@ const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
 
-router.post('/', (req, res) => {
+//async function to get all users from the database and cause there await keyword is used 
+router.post('/',async (req, res) => {
     // try catch block is checking
     // for errors without stopping the server 
     // from running if there is an error in the code 
     try {
-
+        const user = new User(req.body)
+        await user.save(); // save the user to the database 
+        //await keyword is used to wait for the promise to be resolved
+        res.status(201).send({message:'User created successfully'},user);
     }catch(err) {
-        console.log(err);
+        res.status(500).send({message:err});
     }
 })
