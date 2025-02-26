a5c5540b6b933073f4f9ca9f350827e052fcacb5
Fer-012 <aouachriferdaoues@gmail.com>
2025-02-26 09:25:22 +0100

    Refactor user routes for improved readability and add DELETE route for user removal

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,57 +2,71 @@ const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
 
-// Async function to get all users from the database
+
 router.post('/', async (req, res) => {
-    // Try-catch block is checking for errors without stopping the server
     try {
         const user = new User(req.body);
-        await user.save(); // Save the user to the database
-        // Await keyword is used to wait for the promise to be resolved
+        await user.save();
         res.status(201).send({ message: 'User created successfully', user });
     } catch (err) {
         res.status(500).send({ message: err.message });
     }
 });
 
-router.get('/all',async (req,res)=>{
-    try{
+
+router.get('/all', async (req, res) => {
+    try {
         const users = await User.find();
-        res.send(users);
-    }catch(err){
-        res.status(500).send({message:err.message});
+        res.status(200).send(users);
+    } catch (err) {
+        res.status(500).send({ message: err.message });
     }
 });
 
-router.get('/:name',async (req,res)=>{
-    try{
-        const name = req.params.name;
-        const user = await User.findOne({name});
-        if(user){
+
+router.get('/:name', async (req, res) => {
+    try {
+        const user = await User.findOne({ name: req.params.name });
+        if (user) {
             res.status(200).send(user);
-        }else{
-            res.status(404).send({message:"User not found"});
+        } else {
+            res.status(404).send({ message: 'User not found' });
         }
-    }catch(err){
-        res.status(404).send({message:err.message});
+    } catch (err) {
+        res.status(500).send({ message: err.message });
     }
 });
 
-router.put('/:name',async (req,res)=>{
-    try{
-        const name = req.params.name;
-        const user = await
-        User.findOneAndUpdate
-        ({name},req.body,{new:true});
-        if(user){
-            res.status(200).send(user);
-        }else{
-            res.status(404).send({message:"User not found"});
-        }
-    }catch(err){
-        res.status(404).send({message:err.message});
-    }
-})
 
-// Exporting the router to be used in other files
+router.put('/:name', async (req, res) => {
+    try {
+        const user = await User.findOneAndUpdate(
+            { name: req.params.name },
+            req.body,
+            { new: true }
+        );
+        if (user) {
+            res.status(200).send(user);
+        } else {
+            res.status(404).send({ message: 'User not found' });
+        }
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+});
+
+
+router.delete('/:name', async (req, res) => {
+    try {
+        const user = await User.findOneAndDelete({ name: req.params.name });
+        if (user) {
+            res.status(200).send({ message: 'User deleted successfully' });
+        } else {
+            res.status(404).send({ message: 'User not found' });
+        }
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+});
+
 module.exports = router;
